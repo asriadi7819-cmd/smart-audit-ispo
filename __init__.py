fbcde053023b395321c618e0d3c24835f1018e0d
asriadi7819-cmd <asriadi7819@gmail.com>
2026-09-04 16:08:54 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .prinsip_1 import PRINSIP_1_DATA
+from .prinsip_2 import PRINSIP_2_DATA
+from .prinsip_3 import PRINSIP_3_DATA
+from .prinsip_4 import PRINSIP_4_DATA
+from .prinsip_5 import PRINSIP_5_DATA
+from .prinsip_6 import PRINSIP_6_DATA
+from .prinsip_7 import PRINSIP_7_DATA
